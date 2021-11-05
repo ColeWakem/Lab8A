245b7d6d13d540a40e32efba2bacb6b0222cb2d8
colewakem <90348331+ColeWakem@users.noreply.github.com>
2021-11-05 08:17:48 -0700

    g

---
+++ Lab8A.js
@@ -0,0 +1,5 @@
+function mix() {
+  document.getElementById("Blue").style.color = "blue";
+  document.getElementById("Green").style.color = "";;
+  document.getElementById("Red").style.color = "red";;
+}
