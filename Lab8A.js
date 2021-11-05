a5fc58b8277751a11174459c8b3dbad3402bdc79
colewakem <90348331+ColeWakem@users.noreply.github.com>
2021-11-05 08:22:03 -0700

    k

--- Lab8A.js
+++ Lab8A.js
@@ -1,5 +1,5 @@
 function mix() {
-  document.getElementById("Blue").style.color = "blue";
-  document.getElementById("Green").style.color = "";;
-  document.getElementById("Red").style.color = "red";;
+  document.getElementById("blue").style.color = "blue";
+  document.getElementById("green").style.color = "green";
+  document.getElementById("red").style.color = "red";
 }
